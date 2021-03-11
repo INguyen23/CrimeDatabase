4a81e898d39b94c61a06387242586ee62e80dfba
miller3004 <70497957+miller3004@users.noreply.github.com>
2021-03-11 14:33:47 -0500

    Added Stubs to Case

--- CrimeDatabase/Case.java
+++ CrimeDatabase/Case.java
@@ -1,5 +1,48 @@
 package CrimeDatabase;
 
 public class Case {
-    
+    private int id;
+    private Crime level;
+    private Person subject;
+    private Person arrestingOfficer;
+    private String date;
+    private Evidence [] evidence;
+    private String description;
+
+    public void setId() {
+
+    }
+    public void setLevel() {
+
+    }
+    public void setSubject() {
+
+    }
+    public void setDate() {
+
+    }
+    public void setDescription() {
+
+    }
+    public void setEvidence() {
+
+    }
+    public int getId() {
+        return id;
+    }
+    public Crime getLevel() {
+        return level;
+    }
+    public Person getSubject() {
+        return subject;
+    }
+    public String getDate() {
+        return date;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public Evidence [] getEvidence() {
+        return evidence;
+    }
 }
