be05412ce2d3c1eebf0df73c115367def25552b1
miller3004 <70497957+miller3004@users.noreply.github.com>
2021-03-11 14:25:49 -0500

    Creating and adding Driver class, Creating empty Case Class

---
+++ CrimeDatabase/Case.java
@@ -0,0 +1,5 @@
+package CrimeDatabase;
+
+public class Case {
+    
+}
---
+++ CrimeDatabase/Driver.java
@@ -0,0 +1,45 @@
+package CrimeDatabase;
+
+import java.util.ArrayList;
+
+public class Driver {
+    private String accessLevel;
+    private Case[] Cases;
+    private ArrayList<String> logins;
+
+    private void editInfo() {
+
+    }
+
+    private void seachCases() {
+
+    }
+
+    private void addCase() {
+
+    }
+
+    private boolean verifyCredentials(String user, String pass) {
+        return false;
+    }
+
+    private void editCase() {
+
+    }
+
+    private void removeCase() {
+
+    }
+
+    private void addCredentials() {
+
+    }
+
+    private void editCredentials() {
+        
+    }
+
+    public static void main(String [] args) {
+
+    }
+}
