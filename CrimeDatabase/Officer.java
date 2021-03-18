a1b274687abac08fde352bde88980ed3cbb5899e
miller3004 <70497957+miller3004@users.noreply.github.com>
2021-03-18 14:27:29 -0400

    Adding number of arrests to Officer Class

--- CrimeDatabase/Officer.java
+++ CrimeDatabase/Officer.java
@@ -3,10 +3,12 @@ package CrimeDatabase;
 public class Officer extends Person{
     private String name;
     private int age;
+    private int arrests;
 
-    public Officer(String name, int age) {
+    public Officer(String name, int age, int arrests) {
         this.name = name;
         this.age = age;
+        this.arrests =arrests;
     }
 
     public String getName() { 
@@ -17,11 +19,18 @@ public class Officer extends Person{
         return age;
     }
 
+    public int getArrests() {
+        return arrests;
+    }
     public void setName(String name) {
         this.name = name;
     }
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public void setArrests(int arrests){
+        this.arrests=arrests;
     }
 }
